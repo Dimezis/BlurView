f094570fb9640c331479d56e6ec91ba3fe884d0d
Dimezis <Dmitry.Savyuk@gmail.com>
2016-05-02 16:16:44 +0300

    Added destroy and canReuse methods

--- library/src/main/java/eightbitlab/com/blurview/BlurAlgorithm.java
+++ library/src/main/java/eightbitlab/com/blurview/BlurAlgorithm.java
@@ -4,4 +4,11 @@ import android.graphics.Bitmap;
 
 public interface BlurAlgorithm {
     Bitmap blur(Bitmap bitmap, int blurRadius);
+
+    /**
+     * Free your resources here
+     */
+    void destroy();
+
+    boolean canReuseBitmap();
 }
