686c5d1f763418b150cee23117e69a2879e2515f
Fox2Code <fox2code@gmail.com>
2022-10-08 14:11:22 +0200

    Add javadoc to improve code readability.

--- library/src/main/java/eightbitlab/com/blurview/BlurViewCanvas.java
+++ library/src/main/java/eightbitlab/com/blurview/BlurViewCanvas.java
@@ -19,10 +19,21 @@ public class BlurViewCanvas extends Canvas {
         super(bitmap);
     }
 
-    public void saveRoot() {
+    /**
+     * Call normal {@link Canvas#save()} to save current index to
+     * {@link #saveCountInitial}, should only be called by
+     * {@link PreDrawBlurController#updateBlur()}
+     */
+    void saveRoot() {
         saveCountInitial = this.save();
     }
 
+    /**
+     * Override of {@link Canvas#restore()} to avoid
+     * crashed when there is more restores than saves,
+     * use {@link #saveCountInitial} to count save/restore count
+     * so there is no need to override or hook {@link Canvas#save()}
+     */
     @Override
     public void restore() {
         if (getSaveCount() <= saveCountInitial) {
@@ -33,12 +44,22 @@ public class BlurViewCanvas extends Canvas {
         super.restore();
     }
 
+    /**
+     * Override of {@link Canvas#restoreToCount(int)} to avoid
+     * restoring to the original transformation matrix.
+     *
+     * @param saveCount The save level to restore to.
+     */
     @Override
     public void restoreToCount(int saveCount) {
         super.restoreToCount(Math.max(saveCount, saveCountInitial));
     }
 
-    public void restoreRoot() {
+    /**
+     * Restore to {@link #saveCountInitial}, should
+     * only be called by {@link PreDrawBlurController#updateBlur()}
+     */
+    void restoreRoot() {
         if (getSaveCount() > saveCountInitial + 1) {
             Log.e("BlurView", "rootView has more saves than restores");
         }
