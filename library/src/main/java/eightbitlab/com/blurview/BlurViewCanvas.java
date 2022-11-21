650cfe8f1431f546411bb0292f705bebb3c9b0cd
Dimezis <dmitry.savyuk@gmail.com>
2022-11-21 12:48:12 +0100

    Fix typo

--- library/src/main/java/eightbitlab/com/blurview/BlurViewCanvas.java
+++ library/src/main/java/eightbitlab/com/blurview/BlurViewCanvas.java
@@ -5,7 +5,7 @@ import android.graphics.Canvas;
 
 import androidx.annotation.NonNull;
 
-// Servers purely as a marker of a Canvas used in BlurView
+// Serves purely as a marker of a Canvas used in BlurView
 // to skip drawing itself and other BlurViews on the View hierarchy snapshot
 public class BlurViewCanvas extends Canvas {
     public BlurViewCanvas(@NonNull Bitmap bitmap) {
